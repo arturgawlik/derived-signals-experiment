73eed2e48be45d7adb7eee0058bf9f2417dda86f
arturgawlik <artur.gawlik@icloud.com>
2024-09-10 21:07:34 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,55 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, input, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-select',
+  standalone: true,
+  template: `
+    <select
+      #selectRef
+      [value]="state().selectedOption()"
+      (change)="setSelectedOption(selectRef.value)"
+    >
+      @for (option of state().options; track option) {
+      <option id="option">
+        {{ option }}
+      </option>
+      }
+    </select>
+  `,
+})
+export class SelectCmp {
+  options = input<string[]>();
+
+  protected readonly state = computed(() => {
+    return {
+      options: this.options(),
+      selectedOption: signal<string | null>(null),
+    };
+  });
+
+  setSelectedOption(selectedOption: string) {
+    this.state().selectedOption.set(selectedOption);
+  }
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [SelectCmp],
+  template: `
+    <input type="text" #inputRef />
+    <button style="margin-bottom: 100px;" (click)="setOption(inputRef.value)">
+      submit option
+    </button>
+
+    <app-select [options]="options()" />
+  `,
 })
 export class AppComponent {
-  title = 'derived-signals-experiment';
+  optionsStr = signal('');
+  options = computed(() => this.optionsStr().split(';'));
+
+  setOption(optionStr: string) {
+    this.optionsStr.set(optionStr);
+  }
 }
